c9ab78885daa488ba36367f0e578ddac09b47fd9
hayoung <sks05039@naver.com>
2020-06-09 03:39:15 +0900

    naver_gen

--- signup/join/routes/users.js
+++ signup/join/routes/users.js
@@ -111,33 +111,24 @@ router.get("/logout", function(req,res,next){
   res.redirect("/users/beforeLogin_main.html")
 })
 //네이버 제목 검색.
+var client_id = 'BlpJyqmTUhWR1YI1iMTB';
+var client_secret ='SWKEJ1aW1Z';
 router.get('/search/movie', function (req, res) {
-  var api_url = 'https://openapi.naver.com/v1/search/movie?query=' + encodeURI(req.query.query);
+  var api_url = 'https://openapi.naver.com/v1/search/movie?query=' + encodeURI(req.query.query) + '&display=' + encodeURI(req.query.genre);
   var request = require('request');
   var options = {
       url: api_url,
       headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
    };
   request.get(options, function (error, response, body) {
-
     if (!error && response.statusCode == 200) {
-      res.redirect("/users/beforeLogin_naver_genre.html");
-      fs.writeFile('views/users/result/result.json',body,function(err) {
-        if (err === null) { 
-          console.log('success'); 
-        } 
-        else { 
-          console.log('fail');
-          console.log(err); 
-        }
-      })
+      res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
       res.end(body);
     } else {
       res.status(response.statusCode).end();
       console.log('error = ' + response.statusCode);
     }
   });
-
 });
 
 module.exports = router;
